d551693d9ab66952f6d03895304069e4ad19b1c5
liao <liao961120@gmail.com>
2021-06-01 22:22:50 +0800

    tweak

--- main.js
+++ main.js
@@ -1,42 +1,40 @@
 import { Car } from './car.js';  // named import
 import House from './house.js';  // default import
 
-var benz = new Car("benz", 1000);
-benz.addPrice(50).addPrice(10);
-console.log("Benz's price is", benz);
+///// Set up new instances of objects /////
+var myCar = new Car("benz", 1000);
+myCar.addPrice(50).addPrice(10);
+console.log("My car's price is", myCar);
 
 var myHouse = new House("Taipei", 500000);
 console.log("My house is in", myHouse.city);
 console.log("I migrated to", myHouse.migrate("Posong").city);
 
 
-// Add to DOM
+///// Add objects to DOM /////
 var body = document.querySelector("body");
-var benz_node = document.createElement("p");
+var car_node = document.createElement("p");
 var house_node = document.createElement("p");
-benz_node.id = "my-car";
+car_node.id = "my-car";
 house_node.id = "my-house";
-body.appendChild(benz_node);
+body.appendChild(car_node);
 body.appendChild(house_node);
 
-benz_node.innerText = `Benz's price: ${benz.price}`;
+car_node.innerText = `My car's price: ${myCar.price}`;
 house_node.innerText = `My house is in ${myHouse.city}`;
 
-// Hook functions on DOM
+
+///// Hook interactive functions on the DOM /////
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector("button#add-price").onclick = function() {
         const price = document.getElementById("price");
-        benz.addPrice(parseInt(price.value));
-        price.value = 0;
-        // Update DOM
-        benz_node.innerText = `Benz's price: ${benz.price}`; 
+        myCar.addPrice(parseInt(price.value));
+        car_node.innerText = `My car's price: ${myCar.price}`;  // Update DOM
     };
 
     document.querySelector("button#migrate").onclick = function() {
         const city = document.getElementById("city");
         myHouse.migrate(city.value);
-        city.value = "";
-        // Update DOM
-        house_node.innerText = `My house is in ${myHouse.city}`;
+        house_node.innerText = `My house is in ${myHouse.city}`; // Update DOM
     }
 });
